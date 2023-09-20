f3b1804956fd896b3c7332c317be14365d32c3ed
Emmanuel Darkwa <emmanueldarkwaampadu@gmail.com>
2023-09-20 09:24:30 +0000

    landing page text changed

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -15,9 +15,8 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
+            Welcome to Jobster, your #1 job seeking site on the planet. Track all job 
+            applications and get notified in a heartbeat! Sign up now and experience ease.
           </p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
